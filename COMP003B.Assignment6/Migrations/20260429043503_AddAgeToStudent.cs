cba3ae27a07394e32ae474d086e426a9d6dc509c
samian <sam.iannazzo@yahoo.com>
2026-04-28 21:42:52 -0700

    created controller for author

---
+++ COMP003B.Assignment6/Migrations/20260429043503_AddAgeToStudent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace COMP003B.Assignment6.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAgeToStudent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PublishDate",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublishDate",
+                table: "Books");
+        }
+    }
+}
