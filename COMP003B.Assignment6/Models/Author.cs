d5cbf5eb862b553f4f546f262e3e82383e71fe06
samian <sam.iannazzo@yahoo.com>
2026-04-28 19:50:16 -0700

    created model for author

---
+++ COMP003B.Assignment6/Models/Author.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace COMP003B.Assignment6.Models
+{
+    public class Author
+    {
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
