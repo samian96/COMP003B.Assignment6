d30897bf85e6a65ba544b8d52b7a2ae2a60159df
samian <sam.iannazzo@yahoo.com>
2026-04-28 19:52:07 -0700

    added navigation for property collection in author model

--- COMP003B.Assignment6/Models/Author.cs
+++ COMP003B.Assignment6/Models/Author.cs
@@ -3,8 +3,14 @@ namespace COMP003B.Assignment6.Models
 {
     public class Author
     {
+        public int AuthorId { get; set; }
+
         [Required]
         public string Name { get; set; }
+
+        // navigation for property collection
+
+        public virtual ICollection<BookAuthor>? BookAuthors { get; set; }
 
     }
 }
