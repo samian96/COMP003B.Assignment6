af29029c8574244338e975f0c372277e51399226
samian <sam.iannazzo@yahoo.com>
2026-04-28 19:54:54 -0700

    created model for book added required field / added navigation collection

---
+++ COMP003B.Assignment6/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace COMP003B.Assignment6.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        // NAvigation property collection
+
+        public virtual ICollection<BookAuthor>? BookAuthors { get; set; }
+    }
+}
