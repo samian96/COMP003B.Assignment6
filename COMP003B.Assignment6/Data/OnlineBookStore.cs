a03c275325d4799c0986a3686d0cfb38d7d1ef69
samian <sam.iannazzo@yahoo.com>
2026-04-28 20:13:21 -0700

    created data folder/ added OnlineBookStore / added DbContext DbSet for properties

---
+++ COMP003B.Assignment6/Data/OnlineBookStore.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using COMP003B.Assignment6.Models;
+namespace COMP003B.Assignment6.Data
+{
+    public class OnlineBookStore : DbContext
+    {
+        public OnlineBookStore(DbContextOptions<OnlineBookStoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+    }
+}
