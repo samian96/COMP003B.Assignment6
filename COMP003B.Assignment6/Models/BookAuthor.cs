e347f463afe53f650637c8af7071c3f5b6e68648
samian <sam.iannazzo@yahoo.com>
2026-04-28 19:57:26 -0700

    created model for BookAuthor / added public int fields / added nullable properties

---
+++ COMP003B.Assignment6/Models/BookAuthor.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace COMP003B.Assignment6.Models
+{
+    public class BookAuthor
+    {
+        public int Id { get; set; }
+        public int AuthorId { get; set; }
+        public int BookId { get; set; }
+
+        // nullable properties
+
+        public virtual Author? Author { get; set; }
+        public virtual Book? Book { get; set; }
+    }
+}
